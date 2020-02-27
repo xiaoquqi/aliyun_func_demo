2c8be520ca5a0f11dd4fd97819cc964c717a6a29
Ray Sun <ray.sun@oneprocloud.com>
2020-02-27 14:21:19 +0800

    Add retry decorator in BaiduFace class
    As QPS limitation for free user, add retry logical in decorator
    Upload a new person image

--- face_recognition/worker/baidu_face.py
+++ face_recognition/worker/baidu_face.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import time
+from functools import wraps
 
 from aip import AipFace
 
@@ -15,6 +17,44 @@ DEFAULT_OPTIONS = {
     "liveness_control": "LOW"
 }
 
+# NOTE(Ray): Due to baidu free account limitation to QPS, we need retry
+# interface to retry api access.
+RETRY_INTERVAL = 5
+RETRY_TIMES = 5
+
+def retry(ExceptionToCheck,
+          tries=RETRY_TIMES,
+          delay=RETRY_INTERVAL,
+          backoff=2,
+          logger=None):
+
+    def deco_retry(f):
+
+        @wraps(f)
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            while mtries > 1:
+                try:
+                    return f(*args, **kwargs)
+                except ExceptionToCheck, e:
+                    msg = "%s, Retrying in %d seconds..." % (
+                        str(e), mdelay)
+                    if logger:
+                        logger.warning(msg)
+                    else:
+                        logger.info(msg)
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
+            return f(*args, **kwargs)
+
+        return f_retry  # true decorator
+
+    return deco_retry
+
+class QPSLimitation(Exception):
+    pass
+
 class BaiduFace(object):
 
     def __init__(self,
@@ -27,6 +67,7 @@ class BaiduFace(object):
         self.image_type = image_type
         self.options = options
 
+    @retry(QPSLimitation, logger=logging)
     def search_face(self, image_url):
         """Search face in the group"""
         search_options = self.options.copy()
@@ -34,20 +75,28 @@ class BaiduFace(object):
         search_ret = self.client.search(
             image_url, IMAGE_TYPE, DEFAULT_GROUP, search_options)
         logging.info("Search face returns: %s" % search_ret)
+        if search_ret["error_code"] == 18:
+            raise QPSLimitation(search_ret["error_msg"])
         return search_ret["result"]
 
+    @retry(QPSLimitation, logger=logging)
     def add_face(self, image_url, username):
         """Add user into face group"""
         add_ret = self.client.addUser(
             image_url, self.image_type, DEFAULT_GROUP, username)
         logging.info("Add face returns: %s" % add_ret)
+        if add_ret["error_code"] == 18:
+            raise QPSLimitation(add_ret["error_msg"])
         return add_ret["result"]
 
+    @retry(QPSLimitation, logger=logging)
     def detect_face(self, image_url):
         """Detect if the pics contains a person face"""
         detect_ret = self.client.detect(
             image_url, self.image_type, self.options)
         logging.info("Detect face returns: %s" % detect_ret)
+        if detect_ret["error_code"] == 18:
+            raise QPSLimitation(detect_ret["error_msg"])
         return detect_ret["result"]
 
     def is_human_face(self, image_url):
